6a6c153b16a4d44bf8be7c30fb7e46e262b447f7
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-04-12 19:25:20 +0530

    Add files via upload

---
+++ Print Statements and Comments.py
@@ -0,0 +1,10 @@
+# Printing  Greeting Message
+print("Hello,Earth!!!")
+
+#Printing My Name
+print("My Name Is Is Ayaan")
+
+#Printing A Fun Fact About Myself
+print("I Love!!! Playing Basketball !!!")
+
+print("The Result Of 5+9 Is:", 5+9)
