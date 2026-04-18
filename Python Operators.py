1a6295a3a17c5e7cfa3153472160e9eb5da81c54
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-04-18 20:41:27 +0530

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,9 @@
+num1 = 45
+num2 = 3 
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition :",num1 + num2)
+print("Difference :", num1-num2)
+print("Product :", num1 * num2)
+print("Division :", num1 / num2)
