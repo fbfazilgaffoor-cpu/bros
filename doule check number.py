1dde196e3e7a7ee81e8a99aa62b96ce16ead690f
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-04-26 10:35:22 +0530

    Add files via upload

---
+++ doule check number.py
@@ -0,0 +1,11 @@
+num = int(input("Enter Number To Check :"))
+if num>50:
+    print("Number Is Greater Than 50")
+    if num%2==0:
+    print("And It IUs Even Too ")
+
+else:
+    print("And It Is Odd")
+
+else:
+    print("Number Is Less Than 50")
