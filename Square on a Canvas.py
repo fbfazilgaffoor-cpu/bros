db817f391797d9594e2709b20a22d847e3b0244c
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-05-02 20:12:25 +0530

    Add files via upload

---
+++ Square on a Canvas.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+turtle.title("Welcome To Turtle Window")
+board = turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i+1
