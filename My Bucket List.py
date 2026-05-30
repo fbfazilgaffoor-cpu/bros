e245876d448dadac8caeb6e82230a937eaee49e4
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-05-30 20:39:50 +0530

    Add files via upload

---
+++ My Bucket List.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pygame.init()
+
+SPRITE_COLOR_CHANGE_EVENT = pygame.USEREVENT + 1
+BACKGROUND_COLOR_CHANGE_EVENT = pygame.USEREVENT + 2
+
+BLUE = pygame.Color('blue')
+LIGHTBLUE = pygame.Color('lightblue')
+DARKBLUE = pygame.Color('darkblue')
+
+YELLOW = pygame.Color('yellow')
+MAGENTA = pygame.Color('magenta')
+ORANGE = pygame.Color('orange')
+WHITE = pygame.Color('white')
