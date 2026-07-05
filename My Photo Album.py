b9e05a7ba892af200756e5b5a15037f2ac41ff79
fbfazilgaffoor-cpu <fbfazilgaffoor@gmail.com>
2026-07-05 09:13:34 +0530

    Add files via upload

--- My Photo Album.py
+++ My Photo Album.py
@@ -1,34 +1,90 @@
+
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-
+ 
 window = Tk()
-window.title('My Photo Album')
-window.geometry('400x420')
+window.title("My Photo Album")
+window.geometry("450x500")
+window.config(bg="lavender")
+ 
+title_label = Label(
+    window,
+    text="My Photo Album",
+    font=("Arial", 20, "bold"),
+    fg="white",
+    bg="purple",
+    width=25
+)
+title_label.pack(pady=15)
+ 
+image_file = Image.open("photo.jpg")
+image_file = image_file.resize((300, 200))
+ 
+photo = ImageTk.PhotoImage(image_file)
+ 
+image_label = Label(window, image=photo, bg="lavender")
+image_label.pack(pady=10)
 
-title = Label(window, text='My Photo Album', fg='white', bg='purple', width=40)
-title.pack(pady=10)
-img_file = Image.open('dog.jpg')
-img_file = img_file.resize((300, 180))
-photo = ImageTk.PhotoImage(img_file)
-pic = Label(window, image=photo)
-pic.pack(pady=5)
-
-def show_message():
-    messagebox.showinfo('Great!', 'You clicked the photo!')
-msg_btn = Button(window, text='Click to React', bg='blue', fg='white', command=show_message)
-msg_btn.pack(pady=5)
-
-def show_details():
-    top = Toplevel()
-    top.title('Photo Details')
-    top.geometry('200x120')
-    info  = Label(top, text='Taken on: 1 June 2025')
-    info.pack(pady=10)
-    place = Label(top, text='Location: My Garden')
-    place.pack()
-    top.mainloop()
-details_btn = Button(window, text='See Details', bg='green', fg='white', command=show_details)
-details_btn.pack(pady=5)
-
+def show_reaction():
+    messagebox.showinfo(
+        "Photo Reaction",
+        "This is a beautiful memory!"
+    )
+ 
+def open_photo_details():
+    details_window = Toplevel(window)
+    details_window.title("Photo Details")
+    details_window.geometry("350x250")
+    details_window.config(bg="lightyellow")
+ 
+    heading = Label(
+        details_window,
+        text="Photo Details",
+        font=("Arial", 16, "bold"),
+        bg="lightyellow",
+        fg="purple"
+    )
+    heading.pack(pady=15)
+ 
+    details = Label(
+        details_window,
+        text="Photo Name: My Favourite Memory\n"
+             "Category: Personal Album\n"
+             "Description: A special photo saved in my album.",
+        font=("Arial", 11),
+        bg="lightyellow",
+        justify="left"
+    )
+    details.pack(pady=10)
+ 
+    close_button = Button(
+        details_window,
+        text="Close",
+        bg="purple",
+        fg="white",
+        command=details_window.destroy
+    )
+    close_button.pack(pady=15)
+ 
+reaction_button = Button(
+    window,
+    text="React to Photo",
+    font=("Arial", 12, "bold"),
+    bg="blue",
+    fg="white",
+    command=show_reaction
+)
+reaction_button.pack(pady=10)
+ 
+details_button = Button(
+    window,
+    text="View Photo Details",
+    font=("Arial", 12, "bold"),
+    bg="green",
+    fg="white",
+    command=open_photo_details
+)
+details_button.pack(pady=10)
+ 
 window.mainloop()
